e785713d3e8773cbc4ddcec7b912b6e8be7f7fb8
Robert Wagner <robert@wagner.id.au>
2024-07-26 15:33:02 +1000

    Merge fix

--- src/dbup-postgresql/Properties/AssemblyInfo.cs
+++ src/dbup-postgresql/Properties/AssemblyInfo.cs
@@ -2,13 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-
-#if (NETSTANDARD2_0_OR_GREATER  || NET462_OR_GREATER)
 // Npgsql is not CLS compliant and PostgresqlConnectionManager exposes types like NpgsqlDataSource
 [assembly: CLSCompliant(false)]
-#else
-[assembly: CLSCompliant(true)]
-#endif
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5ddc04cc-0bd3-421e-9ae4-9fd0e4f4ef04")]

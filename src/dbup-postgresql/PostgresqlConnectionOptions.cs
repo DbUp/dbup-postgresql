1b2b5b4357f08b7b26be3f5337929429fd35400d
Mark Otter <mark@kaninchen.nl>
2024-12-10 06:50:37 +0100

    Added support for supplying a custom CertificateValidationCallback on the connection (#16)

---
+++ src/dbup-postgresql/PostgresqlConnectionOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DbUp.Postgresql
+{
+    /// <summary>
+    /// Options that will be applied on the created connection
+    /// </summary>
+    public class PostgresqlConnectionOptions
+    {
+        /// <summary>
+        /// Certificate for securing connection.
+        /// </summary>
+        public X509Certificate2 ClientCertificate { get; set; }
+
+        /// <summary>
+        //  Custom handler to verify the remote SSL certificate.
+        //  Ignored if Npgsql.NpgsqlConnectionStringBuilder.TrustServerCertificate is set.
+        /// </summary>
+        public RemoteCertificateValidationCallback UserCertificateValidationCallback { get; set; }
+    }
+}

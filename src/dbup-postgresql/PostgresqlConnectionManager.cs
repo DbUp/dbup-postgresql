49956ad557970788184ddd15b5de9e5d24eab2b5
Robert Wagner <robert@wagner.id.au>
2024-12-10 16:50:20 +1000

    File scoped namespaces and primary ctors

--- src/dbup-postgresql/PostgresqlConnectionManager.cs
+++ src/dbup-postgresql/PostgresqlConnectionManager.cs
@@ -4,79 +4,78 @@ using System.Security.Cryptography.X509Certificates;
 using DbUp.Engine.Transactions;
 using Npgsql;
 
-namespace DbUp.Postgresql
+namespace DbUp.Postgresql;
+
+/// <summary>
+/// Manages PostgreSQL database connections.
+/// </summary>
+public class PostgresqlConnectionManager : DatabaseConnectionManager
 {
     /// <summary>
-    /// Manages PostgreSQL database connections.
+    /// Disallow single quotes to be escaped with a backslash (\')
     /// </summary>
-    public class PostgresqlConnectionManager : DatabaseConnectionManager
+    public bool StandardConformingStrings { get; set; } = true;
+
+    /// <summary>
+    /// Creates a new PostgreSQL database connection.
+    /// </summary>
+    /// <param name="connectionString">The PostgreSQL connection string.</param>
+    public PostgresqlConnectionManager(string connectionString)
+        : base(new DelegateConnectionFactory(l => new NpgsqlConnection(connectionString)))
     {
-        /// <summary>
-        /// Disallow single quotes to be escaped with a backslash (\')
-        /// </summary>
-        public bool StandardConformingStrings { get; set; } = true;
+    }
 
-        /// <summary>
-        /// Creates a new PostgreSQL database connection.
-        /// </summary>
-        /// <param name="connectionString">The PostgreSQL connection string.</param>
-        public PostgresqlConnectionManager(string connectionString)
-            : base(new DelegateConnectionFactory(l => new NpgsqlConnection(connectionString)))
+    /// <summary>
+    /// Creates a new PostgreSQL database connection with a certificate.
+    /// </summary>
+    /// <param name="connectionString">The PostgreSQL connection string.</param>
+    /// <param name="certificate">Certificate for securing connection.</param>
+    public PostgresqlConnectionManager(string connectionString, X509Certificate2 certificate)
+        : this(connectionString, new PostgresqlConnectionOptions
         {
-        }
+            ClientCertificate = certificate
+        }) 
+    {
+    }
 
-        /// <summary>
-        /// Creates a new PostgreSQL database connection with a certificate.
-        /// </summary>
-        /// <param name="connectionString">The PostgreSQL connection string.</param>
-        /// <param name="certificate">Certificate for securing connection.</param>
-        public PostgresqlConnectionManager(string connectionString, X509Certificate2 certificate)
-            : this(connectionString, new PostgresqlConnectionOptions
+    /// <summary>
+    /// Create a new PostgreSQL database connection 
+    /// </summary>
+    /// <param name="connectionString">The PostgreSQL connection string.</param>
+    /// <param name="connectionOptions">Custom options to apply on the created connection</param>
+    public PostgresqlConnectionManager(string connectionString, PostgresqlConnectionOptions connectionOptions)
+        : base(new DelegateConnectionFactory(l =>
             {
-                ClientCertificate = certificate
-            }) 
-        {
-        }
+                NpgsqlConnection databaseConnection = new NpgsqlConnection(connectionString);
+                databaseConnection.ApplyConnectionOptions(connectionOptions);
 
-        /// <summary>
-        /// Create a new PostgreSQL database connection 
-        /// </summary>
-        /// <param name="connectionString">The PostgreSQL connection string.</param>
-        /// <param name="connectionOptions">Custom options to apply on the created connection</param>
-        public PostgresqlConnectionManager(string connectionString, PostgresqlConnectionOptions connectionOptions)
-            : base(new DelegateConnectionFactory(l =>
-                {
-                    NpgsqlConnection databaseConnection = new NpgsqlConnection(connectionString);
-                    databaseConnection.ApplyConnectionOptions(connectionOptions);
+                return databaseConnection;
+            }
+        ))
+    {
+    }
 
-                    return databaseConnection;
-                }
-            ))
-        {
-        }
+    /// <summary>
+    /// Creates a new PostgreSQL database connection with a NpgsqlDatasource
+    /// </summary>
+    /// <param name="datasource">The PostgreSQL NpgsqlDataSource.</param>
+    public PostgresqlConnectionManager(NpgsqlDataSource datasource)
+        : base(new DelegateConnectionFactory(l => datasource.CreateConnection()))
+    {
+    }
 
-        /// <summary>
-        /// Creates a new PostgreSQL database connection with a NpgsqlDatasource
-        /// </summary>
-        /// <param name="datasource">The PostgreSQL NpgsqlDataSource.</param>
-        public PostgresqlConnectionManager(NpgsqlDataSource datasource)
-            : base(new DelegateConnectionFactory(l => datasource.CreateConnection()))
-        {
-        }
+    /// <summary>
+    /// Splits the statements in the script using the ";" character.
+    /// </summary>
+    /// <param name="scriptContents">The contents of the script to split.</param>
+    public override IEnumerable<string> SplitScriptIntoCommands(string scriptContents)
+    {
+        var scriptStatements =
+            PostgresqlQueryParser.ParseRawQuery(scriptContents, StandardConformingStrings)
+                .Select(x => x.Trim())
+                .Where(x => x.Length > 0)
+                .ToArray();
 
-        /// <summary>
-        /// Splits the statements in the script using the ";" character.
-        /// </summary>
-        /// <param name="scriptContents">The contents of the script to split.</param>
-        public override IEnumerable<string> SplitScriptIntoCommands(string scriptContents)
-        {
-            var scriptStatements =
-                PostgresqlQueryParser.ParseRawQuery(scriptContents, StandardConformingStrings)
-                    .Select(x => x.Trim())
-                    .Where(x => x.Length > 0)
-                    .ToArray();
-
-            return scriptStatements;
-        }
+        return scriptStatements;
     }
 }
--- src/dbup-postgresql/PostgresqlObjectParser.cs
+++ src/dbup-postgresql/PostgresqlObjectParser.cs
@@ -1,14 +1,8 @@
 ﻿using DbUp.Support;
 
-namespace DbUp.Postgresql
-{
-    /// <summary>
-    /// Parses Sql Objects and performs quoting functions.
-    /// </summary>
-    public class PostgresqlObjectParser : SqlObjectParser
-    {
-        public PostgresqlObjectParser() : base("\"", "\"")
-        {
-        }
-    }
-}
+namespace DbUp.Postgresql;
+
+/// <summary>
+/// Parses Sql Objects and performs quoting functions.
+/// </summary>
+public class PostgresqlObjectParser() : SqlObjectParser("\"", "\"");

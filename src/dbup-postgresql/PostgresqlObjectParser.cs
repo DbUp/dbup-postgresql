64ac2c97474dfa7f4e43e4d3c6fc9fadc044db34
Robert Wagner <robert@wagner.id.au>
2025-11-05 15:43:16 +1000

    Add XML documentation for all public members

--- src/dbup-postgresql/PostgresqlObjectParser.cs
+++ src/dbup-postgresql/PostgresqlObjectParser.cs
@@ -1,8 +1,11 @@
-﻿using DbUp.Support;
+using DbUp.Support;
 
 namespace DbUp.Postgresql;
 
 /// <summary>
 /// Parses Sql Objects and performs quoting functions.
 /// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="PostgresqlObjectParser"/> class.
+/// </remarks>
 public class PostgresqlObjectParser() : SqlObjectParser("\"", "\"");

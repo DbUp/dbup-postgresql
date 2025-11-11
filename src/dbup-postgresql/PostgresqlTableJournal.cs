521fb894652254478a89b787bea8165c801dc3cb
Aashik Nagadikeri Harish <aashik@StriVR-HQ.local>
2025-11-11 12:46:13 -0800

    Override function to check if table exists

--- src/dbup-postgresql/PostgresqlTableJournal.cs
+++ src/dbup-postgresql/PostgresqlTableJournal.cs
@@ -1,9 +1,10 @@
-using System;
+﻿using System;
 using System.Data;
 using DbUp.Engine;
 using DbUp.Engine.Output;
 using DbUp.Engine.Transactions;
 using DbUp.Support;
+using Npgsql;
 
 namespace DbUp.Postgresql;
 
@@ -73,5 +74,19 @@ public class PostgresqlTableJournal : TableJournal
     applied timestamp without time zone NOT NULL,
     CONSTRAINT {quotedPrimaryKeyName} PRIMARY KEY (schemaversionsid)
 )";
+    }
+
+    /// <inheritdoc/>
+    protected override string DoesTableExistSql()
+    {
+        string fqSchemaTableName = FqSchemaTableName.Replace("'", "''");
+
+        string sql = $@"
+        select case
+            when to_regclass('{fqSchemaTableName}') is not null then 1
+            else 0
+        end;";
+
+        return sql;
     }
 }
